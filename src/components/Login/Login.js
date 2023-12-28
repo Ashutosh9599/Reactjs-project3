afe94e552d08304ff74fa0820ffb9c5751fabb8a
Ashutosh9599 <ashutoshdas091@gmail.com>
2023-12-28 18:16:22 +0530

    Context API

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -65,10 +65,16 @@ const Login = (props) => {
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+
+    // setFormIsValid(
+    //   event.target.value.includes('@') && passwordState.isValid
+    // );
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
+
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
